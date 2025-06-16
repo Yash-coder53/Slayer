2a70492df56adf7b90b58756fb0b463f1ac93961
Sikander <139198327+Yash-coder53@users.noreply.github.com>
2025-06-16 12:10:10 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,32 +1,32 @@
 '''
-=============SON OF GENISYS=====================
-Astra members adding script
-Coded by a dumbass kid- github.com/Cryptonian007
+=============MR SIKANDER=====================
+Slayer members adding script
 Apologies if anything in the code is dumb :)
-Copy with credits
+Who Copy with credits he/she is gay/chinar
 ************************************************
 '''
 
 # import libraries
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerChannel, ChannelParticipantsSearch
-from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError, PhoneNumberBannedError, ChatAdminRequiredError
-from telethon.errors.rpcerrorlist import ChatWriteForbiddenError, UserBannedInChannelError, UserAlreadyParticipantError, FloodWaitError
+from telethon.errors.rpcerrorlist import (
+    PeerFloodError, UserPrivacyRestrictedError, PhoneNumberBannedError,
+    ChatAdminRequiredError, ChatWriteForbiddenError, UserBannedInChannelError,
+    UserAlreadyParticipantError, FloodWaitError
+)
 from telethon.tl.functions.channels import InviteToChannelRequest, GetParticipantsRequest
 import sys
-from telethon.tl.functions.messages import ImportChatInviteRequest, AddChatUserRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest, AddChatUser Request
 from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.tl.types import UserStatusRecently
 import time
 import random
 from colorama import init, Fore
 import os
 import pickle
 
-
 init()
 
-
+# Color definitions
 r = Fore.RED
 lg = Fore.GREEN
 rs = Fore.RESET
@@ -45,43 +45,38 @@ minus = w + '[' + lg + '-' + w + ']' + rs
 def banner():
     # fancy logo
     b = [
-    '   _____             __',
-    '  /  _  \    _______/  |_____________',
-    ' /  /_\  \  /  ___/\   __\_  __ \__  \\',
-    '/    |    \ \___ \  |  |  |  | \// __ \_',
-    '\____|__  /____  >  |__|  |__|  (____   /',
-    '        \/     \/                     \/'
+  ' _____ _         __     ___ _____ _____'
+  '/ ____| |        /\\ \   / /  ____|  __ \'
+ '| (___ | |       /  \\ \_/ /| |__  | |__) |'
+ ' \___ \| |      / /\ \\   / |  __| |  _  /'
+ ' ____) | |____ / ____ \| |  | |____| | \ \'
+' |_____/|______/_/    \_\_|  |______|_|  \_\'
     ]
     for char in b:
         print(f'{random.choice(colors)}{char}{rs}')
-    #print('=============SON OF GENISYS==============')
     print(f'{lg}   Version: {w}1.2{lg} | Author: {w}Cryptonian{rs}\n')
-
 
 # function to clear screen
 def clr():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+# Load accounts from file
 accounts = []
-f = open('vars.txt', 'rb')
-while True:
-    try:
-        accounts.append(pickle.load(f))
-    except EOFError:
-        break
+with open('vars.txt', 'rb') as f:
+    while True:
+        try:
+            accounts.append(pickle.load(f))
+        except EOFError:
+            break
 
-# create sessions(if any) and check for any banned accounts
-# TODO: Remove code input(just to check if an account is banned)
+# Check for banned accounts
 print('\n' + info + lg + ' Checking for banned accounts...' + rs)
+banned = []
 for a in accounts:
     phn = a[0]
     print(f'{plus}{grey} Checking {lg}{phn}')
     clnt = TelegramClient(f'sessions/{phn}', 3910389, '86f861352f0ab76a251866059a6adbd6')
     clnt.connect()
-    banned = []
     if not clnt.is_user_authorized():
         try:
             clnt.send_code_request(phn)
@@ -89,24 +84,21 @@ for a in accounts:
         except PhoneNumberBannedError:
             print(f'{error} {w}{phn} {r}is banned!{rs}')
             banned.append(a)
-    for z in banned:
-        accounts.remove(z)
-        print(info+lg+' Banned account removed[Remove permanently using manager.py]'+rs)
-    time.sleep(0.5)
     clnt.disconnect()
 
+for z in banned:
+    accounts.remove(z)
+    print(info + lg + ' Banned account removed[Remove permanently using manager.py]' + rs)
 
-print(info+' Sessions created!')
+print(info + ' Sessions created!')
 clr()
 banner()
-# func to log scraping details(link of the grp to scrape
-# and current index) in order to resume later
+
+# Function to log scraping details
 def log_status(scraped, index):
     with open('status.dat', 'wb') as f:
         pickle.dump([scraped, int(index)], f)
-        f.close()
     print(f'{info}{lg} Session stored in {w}status.dat{lg}')
-    
 
 def exit_window():
     input(f'\n{cy} Press enter to exit...')
@@ -114,33 +106,30 @@ def exit_window():
     banner()
     sys.exit()
 
-# read user details
+# Read user details
 try:
-    # rquest to resume adding
     with open('status.dat', 'rb') as f:
         status = pickle.load(f)
-        f.close()
-        lol = input(f'{INPUT}{cy} Resume scraping members from {w}{status[0]}{lg}? [y/n]: {r}')
-        if 'y' in lol:
-            scraped_grp = status[0] ; index = int(status[1])
+        resume = input(f'{INPUT}{cy} Resume scraping members from {w}{status[0]}{lg}? [y/n]: {r}')
+        if 'y' in resume:
+            scraped_grp = status[0]
+            index = int(status[1])
         else:
-            if os.name == 'nt': 
-                os.system('del status.dat')
-            else: 
-                os.system('rm status.dat')
+            os.remove('status.dat') if os.name == 'nt' else os.remove('status.dat')
             scraped_grp = input(f'{INPUT}{cy} Public/Private group link to scrape members: {r}')
             index = 0
-except:
+except (FileNotFoundError, EOFError):
     scraped_grp = input(f'{INPUT}{cy} Public/Private group link to scrape members: {r}')
     index = 0
-# load all the accounts(phonenumbers)
+
+# Load all the accounts
 accounts = []
-f = open('vars.txt', 'rb')
-while True:
-    try:
-        accounts.append(pickle.load(f))
-    except EOFError:
-        break
+with open('vars.txt', 'rb') as f:
+    while True:
+        try:
+            accounts.append(pickle.load(f))
+        except EOFError:
+            break
 
 print(f'{info}{lg} Total accounts: {w}{len(accounts)}')
 number_of_accs = int(input(f'{INPUT}{cy} Enter number of accounts to use: {r}'))
@@ -148,29 +137,28 @@ print(f'{info}{cy} Choose an option{lg}')
 print(f'{cy}[0]{lg} Add to public group')
 print(f'{cy}[1]{lg} Add to private group')
 choice = int(input(f'{INPUT}{cy} Enter choice: {r}'))
-if choice == 0:
-    target = str(input(f'{INPUT}{cy} Enter public group link: {r}'))
-else:
-    target = str(input(f'{INPUT}{cy} Enter private group link: {r}'))
+target = input(f'{INPUT}{cy} Enter {"public" if choice == 0 else "private"} group link: {r}')
+
 print(f'{grey}_'*50)
-#status_choice = str(input(f'{INPUT}{cy} Do you wanna add active members?[y/n]: {r}'))
+
 to_use = [x for x in accounts[:number_of_accs]]
 for l in to_use: accounts.remove(l)
+
 with open('vars.txt', 'wb') as f:
     for a in accounts:
         pickle.dump(a, f)
     for ab in to_use:
         pickle.dump(ab, f)
-    f.close()
+
 sleep_time = int(input(f'{INPUT}{cy} Enter delay time per request{w}[{lg}0 for None{w}]: {r}'))
-#print(f'{info}{lg} Joining group from {w}{number_of_accs} accounts...')
-#print(f'{grey}-'*50)
+
 print(f'{success}{lg} -- Adding members from {w}{len(to_use)}{lg} account(s) --')
 adding_status = 0
 approx_members_count = 0
+
 for acc in to_use:
     stop = index + 60
-    c = TelegramClient(f'sessions/{acc[0]}', 3910389 , '86f861352f0ab76a251866059a6adbd6')
+    c = TelegramClient(f'sessions/{acc[0]}', 3910389, '86f861352f0ab76a251866059a6adbd6')
     print(f'{plus}{grey} User: {cy}{acc[0]}{lg} -- {cy}Starting session... ')
     c.start(acc[0])
     acc_name = c.get_me().first_name
@@ -185,7 +173,9 @@ for acc in to_use:
         else:
             c(JoinChannelRequest(scraped_grp))
             print(f'{plus}{grey} User: {cy}{acc_name}{lg} -- Joined group to scrape')
+        
         scraped_grp_entity = c.get_entity(scraped_grp)
+        
         if choice == 0:
             c(JoinChannelRequest(target))
             print(f'{plus}{grey} User: {cy}{acc_name}{lg} -- Joined group to add')
@@ -204,33 +194,33 @@ for acc in to_use:
         print(f'{error}{r} User: {cy}{acc_name}{lg} -- Failed to join group')
         print(f'{error} {r}{e}')
         continue
+
     print(f'{plus}{grey} User: {cy}{acc_name}{lg} -- {cy}Retrieving entities...')
-    #c.get_dialogs()
     try:
         members = []
         while_condition = True
         my_filter = ChannelParticipantsSearch('')
         offset = 0
-        i = 1
         while while_condition:
-            participants = c(GetParticipantsRequest(channel=scraped_grp,  offset= offset, filter = my_filter, limit=200, hash=0))
+            participants = c(GetParticipantsRequest(channel=scraped_grp, offset=offset, filter=my_filter, limit=200, hash=0))
             members.extend(participants.users)
             offset += len(participants.users)
-            members += (participants.users)
-            if len(participants.users) < 1 :
+            if len(participants.users) < 1:
                 while_condition = False
     except Exception as e:
         print(f'{error}{r} Couldn\'t scrape members')
         print(f'{error}{r} {e}')
         continue
+
     approx_members_count = len(members)
     assert approx_members_count != 0
     if index >= approx_members_count:
         print(f'{error}{lg} No members to add!')
         continue
+
     print(f'{info}{lg} Start: {w}{index}')
-    #adding_status = 0
     peer_flood_status = 0
+
     for user in members[index:stop]:
         index += 1
         if peer_flood_status == 10:
@@ -240,16 +230,15 @@ for acc in to_use:
             if choice == 0:
                 c(InviteToChannelRequest(target_details, [user]))
             else:
-                c(AddChatUserRequest(target_details.id, user, 42))
+                c(AddChatUser Request(target_details.id, user, 42))
             user_id = user.first_name
             target_title = target_entity.title
             print(f'{plus}{grey} User: {cy}{acc_name}{lg} -- {cy}{user_id} {lg}--> {cy}{target_title}')
-            #print(f'{info}{grey} User: {cy}{acc_name}{lg} -- Sleep 1 second')
             adding_status += 1
             print(f'{info}{grey} User: {cy}{acc_name}{lg} -- Sleep {w}{sleep_time} {lg}second(s)')
             time.sleep(sleep_time)
         except UserPrivacyRestrictedError:
-            print(f'{minus}{grey} User: {cy}{acc_name}{lg} -- {r}User Privacy Restricted Error')
+            print(f'{minus}{grey} User: {cy}{acc_name}{lg} -- {r}User  Privacy Restricted Error')
             continue
         except PeerFloodError:
             print(f'{error}{grey} User: {cy}{acc_name}{lg} -- {r}Peer Flood Error.')
@@ -267,7 +256,7 @@ for acc in to_use:
             print(f'{error}{grey} User: {cy}{acc_name}{lg} -- {r}Chat Admin rights needed to add')
             continue
         except UserAlreadyParticipantError:
-            print(f'{minus}{grey} User: {cy}{acc_name}{lg} -- {r}User is already a participant')
+            print(f'{minus}{grey} User: {cy}{acc_name}{lg} -- {r}User  is already a participant')
             continue
         except FloodWaitError as e:
             print(f'{error}{r} {e}')
@@ -283,7 +272,7 @@ for acc in to_use:
         except Exception as e:
             print(f'{error} {e}')
             continue
-#global adding_status, approx_members_count
+
 if adding_status != 0:
     print(f"\n{info}{lg} Adding session ended")
 try:
